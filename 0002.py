57f9eef5a28d15d10c568e64d938e8bbcd130af2
DESKTOP-E6BUTSB\Анатолий <tolya.ponomarenko.89@ukr.net>
2024-01-03 16:04:59 +0300

    added conditions theory

--- 0002.py
+++ 0002.py
@@ -1,9 +1,22 @@
-n1, n2 = 10, 20
-print(n1 > n2)
-print(n1 >= n2)
-print(n1 < n2)
-print(n1 <= n2)
-print(n1 == n2)
-print(n1 != n2)
-print(1 == 1 and 3 == 3)
-print(1 == 1 or 2 == 3)
+# n1, n2 = 10, 20
+# print(n1 > n2)
+# print(n1 >= n2)
+# print(n1 < n2)
+# print(n1 <= n2)
+# print(n1 == n2)
+# print(n1 != n2)
+# print(1 == 1 and 3 == 3)
+# print(1 == 1 or 2 == 3)
+# hours = int(input("Enter hours: "))
+# if hours >= 12:
+#     print("PM")
+#     print("asdf")
+# else:
+#     print("AM")
+hours = int(input("Enter hours: "))
+if 12 <= hours < 24:
+        print("PM")
+elif 0 <= hours < 12:
+        print("AM")
+else:
+        print("Incorrect hours!")
